1212d71314c28009b577e72bed0d5c42daf0785c
Niklas Rudemo <niklasrudemo@gmail.com>
2019-02-26 08:00:52 +0100

    First commit

---
+++ counter/views.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+
+from counter.models import Counter
+
+counter_app = Blueprint('counter_app', __name__)
+
+@counter_app.route('/')
+def init():
+    return 'Counter App'
